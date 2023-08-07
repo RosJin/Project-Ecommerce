d8a403473a47a58cfe598bf547fe708828338dd6
RosJin <huyhoang.hn462@gmail.com>
2023-08-07 17:46:25 +0700

    Sort,limit,pagination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -20,7 +20,7 @@ const updateProduct = asyncHandler(async (req, res) => {
         if (req.body.title) {
             req.body.slug = slugify(req.body.title);
         }
-        const updateProduct = await Product.findOneAndUpdate({ _id : id }, req.body, { new: true });
+        const updateProduct = await Product.findOneAndUpdate({ _id: id }, req.body, { new: true });
         res.json(updateProduct);
     } catch (error) {
         throw new Error(error);
@@ -30,7 +30,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 const deleteProduct = asyncHandler(async (req, res) => {
     const id = req.params.id;
     try {
-        const deleteProduct = await Product.findOneAndDelete({_id: id});
+        const deleteProduct = await Product.findOneAndDelete({ _id: id });
         res.json(deleteProduct);
     } catch (error) {
         throw new Error(error);
@@ -49,8 +49,43 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
-        const getAllProduct = await Product.find();
-        res.json(getAllProduct);
+        // Filtering
+        const queryObj = { ...req.query };
+        const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach((el) => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort("-createdAt");
+        }
+
+        // limiting the fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query = query.select("-__v");
+        }
+
+        // pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+        }
+
+        const product = await query;
+        res.json(product);
     } catch (error) {
         throw new Error(error);
     }
