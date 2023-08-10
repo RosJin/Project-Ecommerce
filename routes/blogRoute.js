9fa24ce63308937e17fd4e3bd4502f8b9f361056
RosJin <huyhoang.hn462@gmail.com>
2023-08-10 19:24:47 +0700

    blog

---
+++ routes/blogRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
+const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, liketheBlog, disliketheBlog } = require('../controller/blogCtrl')
+const router = express.Router()
+
+router.post("/", authMiddleware, isAdmin, createBlog)
+router.put("/likes", authMiddleware, isAdmin, liketheBlog)
+router.put("/dislikes", authMiddleware, isAdmin, disliketheBlog)
+router.put("/:id", authMiddleware, isAdmin, updateBlog)
+router.get("/:id", getBlog)
+router.get("/", getAllBlogs)
+router.delete("/:id",authMiddleware, isAdmin, deleteBlog)
+
+
+module.exports = router 
