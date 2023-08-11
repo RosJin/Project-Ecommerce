1f5fdd23159c03018f8d314b8a24a4cfff72eead
RosJin <huyhoang.hn462@gmail.com>
2023-08-11 18:57:46 +0700

    blogcat

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/prodcategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
 const cookieParser = require("cookie-parser")
 const morgan = require("morgan")
 dbConnect();
@@ -22,6 +23,8 @@ app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter)
 app.use("/api/category", categoryRouter)
+app.use("/api/blogcategory", blogcategoryRouter)
+
 
 
 app.use(notFound);
--- routes/blogCatRoute.js
+++ routes/blogCatRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createCategory, updateCategory, deleteCategory, getCategory, getallCategory } = require("../controller/prodcategoryCtrl");
+const { createCategory, updateCategory, deleteCategory, getCategory, getallCategory } = require("../controller/blogCatCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
